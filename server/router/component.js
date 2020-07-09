36fa2d3e3d21a22414b0714874a8e3ce99db637f
smile <2230550672@qq.com>
2020-07-09 14:09:51 +0800

    数据接口更新

--- server/router/component.js
+++ server/router/component.js
@@ -3,7 +3,7 @@ const component = require('../middleware/component')
 const componentApp = express()
 
 componentApp.get('/', component.getComponent,(req,res) =>{
-    res.send(req.data)
+    res.send({data:req.data})
 })
 
 
