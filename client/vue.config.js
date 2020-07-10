b52be25846e8e114ce6148708f30fd2f2a58a10d
smile <2230550672@qq.com>
2020-07-10 08:26:41 +0800

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,19 +1,15 @@
 module.exports = {
     devServer: {
-        proxy: {
-            '/api': {
-              target: 'http://localhost:3000',
-              changeOrigin: true, // 允许跨域
-              pathRewrite: { // 重写路径
-                '^/api': ''
-              }
+      port: 80,
+      host: '127.0.0.1',
+      proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            pathRewrite: {
+              '^/api': ''
             }
-        }
-        // proxy: 'http://localhost:3000'
+          }
+      }
     }
-    // devServer: {
-    //     proxy:"http://127.0.0.1:3000", //开发环境的跨域问题解决，后端springboot服务ip 和 端口
-    //     port: 8080, //dev模式下的前端服务端口，也就是浏览器通过localhost：8085 访问前端页面
-    //     host: '127.0.0.1',
-    // },
 }
